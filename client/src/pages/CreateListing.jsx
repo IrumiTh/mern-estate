53d208a7a2fa1da548da2788cfb283790839c8ff
irumi <irumiabeywickrama@gmail.com>
2024-05-30 21:55:54 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -179,7 +179,10 @@ export default function createListing() {
                     <input type='number' id='regularPrice' min='50' max='100000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.regularPrice} />
                     <div className='flex flex-col items-center'>
                         <p>Regular price</p>
-                        <span className='text-sm'>($ / month)</span>
+                        
+                        {formData.type === 'rent' && (
+                            <span className='text-sm'>($ / month)</span>
+                        )}
                     </div>
                     
                 </div>
@@ -188,7 +191,9 @@ export default function createListing() {
                     <input type='number' id='discountPrice' min='0' max='100000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.discountPrice}/>
                     <div className='flex flex-col items-center'>
                         <p>Discounted Price</p>
-                        <span className='text-sm'>($ / month)</span>
+                        {formData.type === 'rent' && (
+                            <span className='text-sm'>($ / month)</span>
+                        )}
                     </div>
                     
                 </div>
