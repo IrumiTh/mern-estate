1c74735bc3260829c097f5d3a4efa40bddae072a
irumi <irumiabeywickrama@gmail.com>
2024-06-05 16:51:21 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Magestic Estate</h1>
+      <p className='mb-4 text-slate-700'>
+      Welcome to Majestic Estate, your premier destination for finding the perfect place to call home. At Majestic Estate, we understand that choosing a home is one of the most significant decisions in life. That's why we are dedicated to offering an extensive selection of properties that cater to various needs and preferences. Whether you are looking for a cozy apartment in the city, a spacious family home in the suburbs, or a serene countryside retreat, our comprehensive listings and expert guidance make the process seamless and enjoyable.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Our commitment to excellence extends beyond our property listings. At Majestic Estate, we pride ourselves on our exceptional customer service and industry expertise. Our team of experienced real estate professionals is always ready to assist you, providing personalized advice and insights to help you make informed decisions. We believe in building lasting relationships with our clients, ensuring that your journey to finding the perfect home is both rewarding and stress-free.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      At Majestic Estate, we also emphasize the importance of innovation and technology in real estate. Our user-friendly website and advanced search tools enable you to explore properties effortlessly, with detailed information and high-quality images at your fingertips. Whether you are buying, selling, or renting, Majestic Estate is here to support you every step of the way, turning your real estate dreams into reality. Thank you for choosing Majestic Estate – where your perfect place awaits.
+      </p>
     </div>
   )
 }
